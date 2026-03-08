04d716067a23659fce6057d40b7e49b46e1c146c
Tristan-Cabato <tcabato@addu.edu.ph>
2026-03-09 02:07:21 +0800

    Auth API Fix 13

--- AuthApp/lib/auth.ts
+++ AuthApp/lib/auth.ts
@@ -25,7 +25,7 @@ export const authOptions = {
     ],
                    
     secret: process.env.BETTER_AUTH_SECRET,
-    baseURL: process.env.BETTER_AUTH_URL || process.env.VERCEL_URL
+    baseURL: process.env.VERCEL_URL || process.env.BETTER_AUTH_URL
 } satisfies BetterAuthOptions;
 
 export const auth = betterAuth(authOptions);
