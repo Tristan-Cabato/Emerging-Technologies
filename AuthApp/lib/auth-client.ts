53ac7da5b4fcae8509afbacecf7f17cb896b887b
Tristan-Cabato <tcabato@addu.edu.ph>
2026-03-09 01:43:00 +0800

    Auth API Fix 9

--- AuthApp/lib/auth-client.ts
+++ AuthApp/lib/auth-client.ts
@@ -1,7 +1,7 @@
 // lib/auth-client.ts
 import { createAuthClient } from "better-auth/react"; // <--- This sub-path is built into 'better-auth'
 import { inferAdditionalFields } from "better-auth/client/plugins";
-import type { authOptions } from "./auth";
+import { authOptions } from "./auth";
 
 export const authClient = createAuthClient({
     baseURL: typeof window !== "undefined" ? window.location.origin : process.env.NEXT_PUBLIC_APP_URL || "https://authenticationapp-vert.vercel.app",
